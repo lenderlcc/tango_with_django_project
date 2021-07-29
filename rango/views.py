1c15a383343127668fac997bda609e0388843bd6
Firstname Surname <lenderlcc@outlook.com>
2021-07-29 19:21:37 +0800

    chapter10

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,7 @@ from rango.forms import UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 def index(request):
@@ -20,12 +21,42 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    # context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    # request.session.set_test_cookie()
+    visitor_cookie_handler(request)
+    # context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
-    return render(request, 'rango/index.html', context=context_dict)
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+    
 
 def about(request):
+    context_dict = {}
+    visitor_cookie_handler(request)
     context_dict = {'boldmessage': 'This tutorial has been put together by CHEN LU'}
+    context_dict['visits'] = request.session['visits']
+    # if request.session.test_cookie_worked():
+    #     print("TEST COOKIE WORKED!")
+    #     request.session.delete_test_cookie()
     return render(request, 'rango/about.html', context=context_dict)
 
 
